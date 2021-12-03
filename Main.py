ad6961f25dbdbea46903c9537e5c4645814f4bbf
nbagass <narukamibagaskara@gmail.com>
2021-12-03 22:55:11 +0700

    membuat dasar aplikasi sistem informasi klinik x

---
+++ Main.py
@@ -0,0 +1,12 @@
+from numpy import left_shift
+
+
+print("\nSelamat Mengakses Layanan Aplikasi dari Klinik X")
+
+print("================================================\n")
+print("Daftar layanan yang akan digunakan\n"+
+    "1. Daftar & Doktor\n"+
+    "2. Booking jadwal praktik\n\n")
+print("Silahkan pilih opsi: ")
+choice=input()
+

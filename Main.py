65e5a98a9fec055268e2d1285d7ba04974c1b853
nbagass <narukamibagaskara@gmail.com>
2021-12-03 22:59:33 +0700

    menambahkan data list nama & jadwal doctor

--- Main.py
+++ Main.py
@@ -1,11 +1,73 @@
 from numpy import left_shift
+import pandas as pd
+
+doctor ={
+    'NAMA' :[
+        'Dr.Faiz Ramadhan',
+        'Dr.Amelia Putri',
+        'Dr.Alifian Reza'
+    ],
+    'SPESIALIS' :[
+        'Anak',
+        'Ibu Hamil',
+        'Umum'
+    ]
+}
+
+jadwal_0 ={
+    'HARI' :[
+        'Senin',
+        'Rabu',
+        'Jumat'
+    ],
+    'Jam':[
+        '08.00 - 17.00 WIB',
+        '08.00 - 17.00 WIB',
+        '08.00 - 11.00 WIB'
+    ],
+}
+
+jadwal_1 ={
+    'HARI' :[
+        'Selasa',
+        'Kamis',
+        'Jumat'
+    ],
+    'Jam':[
+        '08.00 - 17.00 WIB',
+        '08.00 - 17.00 WIB',
+        '08.00 - 11.00 WIB'
+    ],
+}
+
+jadwal_2 ={
+    'HARI' :[
+        'Senin',
+        'Selasa',
+        'Rabu',
+        'Kamis',
+        'Jumat'
+    ],
+    'Jam':[
+        '08.00 - 17.00 WIB',
+        '08.00 - 17.00 WIB',
+        '08.00 - 17.00 WIB',
+        '08.00 - 17.00 WIB',
+        '08.00 - 11.00 WIB'
+    ],
+}
+
+doctor_table = pd.DataFrame(doctor)
+jadwal0_table = pd.DataFrame(jadwal_0)
+jadwal1_table = pd.DataFrame(jadwal_1)
+jadwal2_table = pd.DataFrame(jadwal_2)
 
 
 print("\nSelamat Mengakses Layanan Aplikasi dari Klinik X")
 
 print("================================================\n")
 print("Daftar layanan yang akan digunakan\n"+
-    "1. Daftar & Doktor\n"+
+    "1. Daftar & Jadwal Doktor\n"+
     "2. Booking jadwal praktik\n\n")
 print("Silahkan pilih opsi: ")
 choice=input()

457a20083a5f3c1ca21b7840e99eb2389e6b629c
nbagass <narukamibagaskara@gmail.com>
2021-12-03 23:16:02 +0700

    menambahkan prototype fitur login

--- Main.py
+++ Main.py
@@ -46,16 +46,14 @@ jadwal_2 ={
         'Selasa',
         'Rabu',
         'Kamis',
-        'Jumat',
-        'ddd'
+        'Jumat'
     ],
     'Jam':[
         '08.00 - 17.00 WIB',
         '08.00 - 17.00 WIB',
         '08.00 - 17.00 WIB',
         '08.00 - 17.00 WIB',
-        '08.00 - 11.00 WIB',
-        'ddd'
+        '08.00 - 11.00 WIB'
     ],
 }
 
@@ -64,28 +62,35 @@ jadwal0_table = pd.DataFrame(jadwal_0)
 jadwal1_table = pd.DataFrame(jadwal_1)
 jadwal2_table = pd.DataFrame(jadwal_2)
 
+username ='proto'
+password = 'password'
+usernameInput = input("Username :")
+passwordInput = input("Password :")
 
-print("\nSelamat Mengakses Layanan Aplikasi dari Klinik X")
+if(passwordInput==password and usernameInput==username):
 
-print("================================================\n")
-print("Daftar layanan yang akan digunakan\n"+
-    "1. Daftar & Jadwal Doktor\n"+
-    "2. Booking jadwal praktik\n\n")
-print("Silahkan pilih opsi: ")
-choice=input()
+    print("\nSelamat Mengakses Layanan Aplikasi dari Klinik X")
 
-if(choice == "1"):
-    print("\n===List Doktor Klinik X===")
-    print(doctor_table.head())
-    print("\nPilih untuk mengetahui jadwal praktik doktor")
-    choice = input()
+    print("================================================\n")
+    print("Daftar layanan yang akan digunakan\n"+
+        "1. Daftar & Jadwal Doktor\n"+
+        "2. Booking jadwal praktik\n\n")
+    print("Silahkan pilih opsi: ")
+    choice=input()
 
-    if(choice == "0"):
-        print(jadwal0_table)
-    elif (choice =="1"):
-        print(jadwal1_table)
-    elif (choice =="2"):
-        print(jadwal2_table)
-else:
-    print("Mohon maaf, opsi yang anda pilih masih dalam tahap pengembangan")
+    if(choice == "1"):
+        print("\n===List Doktor Klinik X===")
+        print(doctor_table.head())
+        print("\nPilih untuk mengetahui jadwal praktik doktor")
+        choice = input()
 
+        if(choice == "0"):
+            print(jadwal0_table)
+        elif (choice =="1"):
+            print(jadwal1_table)
+        elif (choice =="2"):
+            print(jadwal2_table)
+    else:
+        print("Mohon maaf, opsi yang anda pilih masih dalam tahap pengembangan")
+else :
+    print("Mohon maaf username atau password anda salah, Silahkan coba kembali")

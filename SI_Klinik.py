03f5813385bc3a9519b317d202904dc0ee1a64c1
nbagass <narukamibagaskara@gmail.com>
2021-12-04 15:15:16 +0700

    menambahkan fitur username password dan lihat data penyimpanan

--- SI_Klinik.py
+++ SI_Klinik.py
@@ -23,20 +23,49 @@ pasien ={
     ]
 }
 
+penyimpanan ={
+    'Barang':[
+        'Kasur Pasien',
+        'Stetoskop',
+        'Termometer'
+    ],
+    'Jumlah':[
+        '25',
+        '50',
+        '100',
+    ]
+}
+
 pasien_table = pd.DataFrame(pasien)
+penyimpanan_table = pd.DataFrame(penyimpanan)
 
-print("="*35)
-print ("Layanan Sistem Informasi Klinik X :")
-print("="*35)
-print ("1. Pasien\n"+
-        "2. Penyimpanan\n"+
-        "3. Janji Temu\n"+
-        "4. Daftar Obat\n"+
-        "5. Profil\n")
-print("Silahkan pilih opsi : ")
-choice = input()
 
-if(choice == "1"):
-    print(pasien_table)
+username ='proto'
+password = 'password'
+
+print("="*20)
+usernameInput = input("Username :")
+passwordInput = input("Password :")
+print("="*20)
+
+if(passwordInput==password and usernameInput==username):
+
+    print("="*35)
+    print ("Layanan Sistem Informasi Klinik X :")
+    print("="*35)
+    print ("1. Pasien\n"+
+            "2. Penyimpanan\n"+
+            "3. Janji Temu\n"+
+            "4. Daftar Obat\n"+
+            "5. Profil\n")
+    print("Silahkan pilih opsi : ")
+    choice = input()
+
+    if(choice == "1"):
+        print(pasien_table)
+    elif(choice == "2"):
+        print(penyimpanan_table)
+    else:
+        print("Input yang anda masukkan salah")
 else:
-    print("Input yang anda masukkan salah")
+     print("Mohon maaf username atau password anda salah, Silahkan coba kembali")

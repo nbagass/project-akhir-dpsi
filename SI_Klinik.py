5cf7a30d06b17fe538902bf86df6b75e92641099
nbagass <narukamibagaskara@gmail.com>
2021-12-04 15:02:18 +0700

    menambahkan tampilan awal untuk SI_Klinik

---
+++ SI_Klinik.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+print("="*35)
+print ("Layanan Sistem Informasi Klinik X :")
+print("="*35)
+print ("1. Pasien\n"+
+        "2. Penyimpanan\n"+
+        "3. Janji Temu\n"+
+        "4. Daftar Obat\n"+
+        "5. Profil\n")
